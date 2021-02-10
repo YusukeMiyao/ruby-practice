7e73c87a4189d871c3fbfee463084d7ed87639f5
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-02-11 00:29:04 +0900

    クラス、オブジェクト、アクセサ

--- ruby.rb
+++ ruby.rb
@@ -42,3 +42,54 @@ end
 
 avarage_result = avarage(9 ,4 ,2)
 p avarage_result
+
+class Student
+    def initialize(name)  
+        @name = name
+    end
+
+    def avg(math, english)
+        return (math + english) / 2
+    end
+
+    attr_accessor :name
+end
+
+a001 = Student.new("sato")
+p a001.name,a001.avg(70, 80)
+
+class Student
+    def initialize(name)
+        @name = name
+    end
+
+    def cal_avg(data)
+        sum = 0
+        for score in data do
+            sum += score
+        end
+        avg = sum / data.length
+        return avg
+    end
+
+    def jedge (avg)
+        result = ''
+        if 60 <= avg
+            result = 'passed'
+        else
+            result = 'falsed'
+        end
+        return result
+    end
+
+    attr_accessor :name
+    
+end
+
+a001 = Student.new('sato')
+data = [70, 65, 50, 90, 30]
+avg = a001.cal_avg(data)
+result = a001.jedge(avg)
+p a001.name
+p avg
+p result
